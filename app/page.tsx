eb5d522e598d21b2603e4cc4150b9b8a10d621eb
ManzzyGacor <manzz9293@gmail.com>
2026-01-28 23:22:43 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar"
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="p-6">
+        <h2 className="text-2xl font-bold">Welcome to ManzzSpace 🚀</h2>
+      </main>
+    </>
+  )
+}
+
