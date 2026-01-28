95973b8df9a585e42eecbf64f9b229ec0329fafe
ManzzyGacor <manzz9293@gmail.com>
2026-01-28 23:47:19 +0700

    Create Comment.ts

---
+++ lib/models/Comment.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+const CommentSchema = new mongoose.Schema({
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  content: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+export default mongoose.models.Comment ||
+  mongoose.model("Comment", CommentSchema)
