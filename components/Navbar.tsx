668bf46f0b01c623a640e5e4821febce4c9113cd
ManzzyGacor <manzz9293@gmail.com>
2026-01-28 23:40:29 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,27 @@
 
+import Link from "next/link"
+import ThemeToggle from "./ThemeToggle"
+
+export default function Navbar() {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "16px",
+        borderBottom: "1px solid #ddd",
+      }}
+    >
+      <strong>ManzzSpace</strong>
+
+      <div style={{ display: "flex", gap: "12px" }}>
+        <Link href="/">Home</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/admin">Admin</Link>
+        <Link href="/login">Login</Link>
+
+        <ThemeToggle />
+      </div>
+    </nav>
+  )
+}
