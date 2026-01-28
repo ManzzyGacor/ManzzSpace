15f62f0830953affd897bbd230429fe6ee583638
ManzzyGacor <manzz9293@gmail.com>
2026-01-28 23:37:09 +0700

    Create route.ts

---
+++ app/api/comments/route.ts
@@ -0,0 +1,21 @@
+import { getToken } from "next-auth/jwt"
+import { connectDB } from "@/lib/mongodb"
+import Comment from "@/lib/models/Comment"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+  const token = await getToken({ req: req as any })
+  if (!token) {
+    return NextResponse.json({ error: "Unauthorized" })
+  }
+
+  const body = await req.json()
+  await connectDB()
+
+  const comment = await Comment.create({
+    ...body,
+    userId: token.sub,
+  })
+
+  return NextResponse.json(comment)
+}
